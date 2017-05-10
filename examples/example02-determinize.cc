27bdde9de8099b9514e709762220f8219169ae07
Ondra Lengal <ondra.lengal@gmail.com>
2017-05-10 14:25:58 +0200

    examples/: add a few

---
+++ examples/example02-determinize.cc
@@ -0,0 +1,34 @@
+// example02.cc - determinizing an automaton
+
+#include <vata2/nfa.hh>
+#include <iostream>
+
+using namespace Vata2::Nfa;
+
+int main()
+{
+	Nfa aut;
+	Nfa determ;
+
+	aut.initialstates = {1, 3};
+	aut.finalstates = {5};
+	aut.add_trans(1, 'a', 3);
+	aut.add_trans(1, 'a', 10);
+	aut.add_trans(1, 'b', 7);
+	aut.add_trans(3, 'a', 7);
+	aut.add_trans(3, 'b', 9);
+	aut.add_trans(9, 'a', 9);
+	aut.add_trans(7, 'b', 1);
+	aut.add_trans(7, 'a', 3);
+	aut.add_trans(7, 'c', 3);
+	aut.add_trans(10, 'a', 7);
+	aut.add_trans(10, 'b', 7);
+	aut.add_trans(10, 'c', 7);
+	aut.add_trans(7, 'a', 5);
+	aut.add_trans(5, 'a', 5);
+	aut.add_trans(5, 'c', 9);
+
+	determinize(&determ, &aut);
+
+	std::cout << serialize_vtf(&determ);
+}
---
+++ examples/example03-lang-empty.cc
@@ -0,0 +1,33 @@
+// example03 - testing language emptiness
+
+#include <vata2/nfa.hh>
+#include <iostream>
+
+using namespace Vata2::Nfa;
+
+int main()
+{
+	Nfa aut;
+
+	std::cout << "Language empty: " << is_lang_empty(&aut) << "\n";
+
+	aut.initialstates = {1, 3};
+	aut.finalstates = {5};
+	aut.add_trans(1, 'a', 3);
+	aut.add_trans(1, 'a', 10);
+	aut.add_trans(1, 'b', 7);
+	aut.add_trans(3, 'a', 7);
+	aut.add_trans(3, 'b', 9);
+	aut.add_trans(9, 'a', 9);
+	aut.add_trans(7, 'b', 1);
+	aut.add_trans(7, 'a', 3);
+	aut.add_trans(7, 'c', 3);
+	aut.add_trans(10, 'a', 7);
+	aut.add_trans(10, 'b', 7);
+	aut.add_trans(10, 'c', 7);
+	aut.add_trans(7, 'a', 5);
+	aut.add_trans(5, 'a', 5);
+	aut.add_trans(5, 'c', 9);
+
+	std::cout << "Language empty: " << is_lang_empty(&aut) << "\n";
+}
