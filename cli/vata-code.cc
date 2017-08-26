360a58fe4e157da67186c57db231489c74c80d14
Ondra Lengal <ondra.lengal@gmail.com>
2017-08-26 11:18:49 +0200

    cli/: show version number + git SHA

--- cli/vata-code.cc
+++ cli/vata-code.cc
@@ -1,12 +1,17 @@
 // TODO: add header
 
 #include <cstdlib>
+#include <cstring>
 #include <fstream>
 
 #include <vata2/util.hh>
 
 #include "../3rdparty/args.hxx"
 
+/// declaration of external identifiers
+extern const char* VATA_VERSION;
+extern const char* VATA_GIT_SHA;
+extern const char* VATA_GIT_DESCRIBE;
 
 void interpret_input(const std::istream& is);
 
@@ -14,7 +19,7 @@ void interpret_input(const std::istream& is);
 /// The entry point
 int main(int argc, const char* argv[])
 {
-	args::ArgumentParser parser("An interface to the libVATA2 automata library");
+	args::ArgumentParser parser("A CLI interface to the libVATA2 automata library");
 	args::HelpFlag flag_help(parser, "help", "Display this help menu", {'h', "help"});
 	args::Flag flag_version(parser, "version", "test flag", {'v', "version"});
 	args::Positional<std::string> pos_inputfile(parser,
@@ -36,7 +41,19 @@ int main(int argc, const char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	if (pos_inputfile)
+	if (flag_version)
+	{
+		std::cout << "vata-code version " << VATA_VERSION;
+		std::cout << " (" << VATA_GIT_DESCRIBE << ")";
+
+		size_t git_sha_len = std::strlen(VATA_GIT_SHA);
+		std::string git_sha_crop =
+			(git_sha_len < 8)? VATA_GIT_SHA : std::string(VATA_GIT_SHA, 8);
+
+		std::cout << " [git: " << git_sha_crop << "]";
+		std::cout << "\n";
+	}
+	else if (pos_inputfile)
 	{
 		std::string filename = args::get(pos_inputfile);
 		std::fstream fs(filename, std::ios::in);
