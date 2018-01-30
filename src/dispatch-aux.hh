f5c0852f26daf99bfb593aaf04c62b7400ea45c0
Ondra Lengal <ondra.lengal@gmail.com>
2018-01-30 21:26:11 +0100

    dispatch-aux.hh: add header

--- src/dispatch-aux.hh
+++ src/dispatch-aux.hh
@@ -1,4 +1,19 @@
-// TODO: header
+/* dispatch.aux -- auxiliary functions for virtual machine dispatcher
+ *
+ * Copyright (c) 2018 Ondrej Lengal <ondra.lengal@gmail.com>
+ *
+ * This file is a part of libvata2.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #ifndef _VATA2_DISPATCH_AUX_HH_
 #define _VATA2_DISPATCH_AUX_HH_
