840735406eb1bfab2c7cfa276a51550200c2e33a
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2022-06-27 17:49:36 +0200

    bindings/python: rename cpp classes

--- bindings/python/pynfa.pxd
+++ bindings/python/pynfa.pxd
@@ -1,14 +1,14 @@
 from libcpp cimport bool
 
 cdef extern from "vata2/nfa.hh" namespace "Vata2::Nfa":
-    cdef cppclass Trans:
+    cdef cppclass CTrans "Vata2::Nfa::Trans":
         int src
         int symb
         int tgt
-        Trans(int, int, int) except +
+        CTrans(int, int, int) except +
 
-    cdef cppclass Nfa:
-        Nfa() except +
+    cdef cppclass CNfa "Vata2::Nfa::Nfa":
+        CNfa() except +
         void add_trans(int, int, int)
         bool has_trans(int, int, int)
         bool trans_empty()
--- bindings/python/pynfa.pyx
+++ bindings/python/pynfa.pyx
@@ -1,20 +1,20 @@
 cimport pynfa
 from libcpp cimport bool
 
-cdef class PyTrans():
-    cdef pynfa.Trans *thisptr
+cdef class Trans():
+    cdef pynfa.CTrans *thisptr
 
     def __cinit__(self, int a, int b, int c):
-        self.thisptr = new pynfa.Trans(a, b, c)
+        self.thisptr = new pynfa.CTrans(a, b, c)
 
     def __dealloc__(self):
         del self.thisptr
 
-cdef class PyNfa():
-    cdef pynfa.Nfa *thisptr
+cdef class Nfa():
+    cdef pynfa.CNfa *thisptr
 
     def __cinit__(self):
-        self.thisptr = new pynfa.Nfa()
+        self.thisptr = new pynfa.CNfa()
 
     def __dealloc__(self):
         del self.thisptr
