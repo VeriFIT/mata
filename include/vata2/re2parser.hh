6dde6784abf3d414d10adbbfa69bba84e713280d
Martin Hruska <hruskamartin25@gmail.com>
2022-07-13 13:02:54 +0200

    added proper headers

--- include/vata2/re2parser.hh
+++ include/vata2/re2parser.hh
@@ -1,6 +1,19 @@
-//
-// Created by HOM on 21.01.2022.
-//
+/* re2parser.hh -- parser transforming re2 regular expressions to our Nfa
+ *
+ * Copyright (c) 2022 Michal Horky
+ *
+ * This file is a part of libvata2.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #ifndef AUTOMATA_LIBRARY_PARSER_HPP
 #define AUTOMATA_LIBRARY_PARSER_HPP
