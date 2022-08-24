37d0a0205e3ab1b913cb2d4d515633f303207fdf
jurajsic <juraj.sic@outlook.com>
2022-08-24 12:15:20 +0200

    some comments, slight optimization in trim

--- include/mata/noodlify.hh
+++ include/mata/noodlify.hh
@@ -33,14 +33,14 @@ namespace SegNfa
  * @brief Create noodles from segment automaton @p aut.
  *
  * Segment automaton is a chain of finite automata (segments) connected via ε-transitions.
- * A noodle is a copy of the segment automaton with exactly one ε-transition between each two consecutive segments.
+ * A noodle is a vector of pointers to copy of the segmens automata created as if there was exactly one ε-transition between each two consecutive segments.
  *
  * @param[in] automaton Segment automaton to noodlify.
  * @param[in] epsilon Epsilon symbol to noodlify for.
  * @param[in] include_empty Whether to also include empty noodles.
  * @return A list of all (non-empty) noodles.
  */
-std::vector<std::vector<std::shared_ptr<Nfa>>> noodlify(const SegNfa& aut, Symbol epsilon);
+std::vector<std::vector<std::shared_ptr<Nfa>>> noodlify(const SegNfa& aut, Symbol epsilon, bool include_empty = false);
 
 /**
  * @brief Create noodles for left and right side of equation.
