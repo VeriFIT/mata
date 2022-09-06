e01f68eda73bee0f7d80b6fe9b1023879fbca307
David Chocholatý <chocholaty.david@protonmail.com>
2022-09-06 12:34:01 +0200

    Optimize linear search for epsilon in post()

--- include/mata/nfa.hh
+++ include/mata/nfa.hh
@@ -678,19 +678,7 @@ public:
     void print_to_DOT(std::ostream &outputStream) const;
     static Nfa read_from_our_format(std::istream &inputStream);
 
-    StateSet post(const StateSet& states, const Symbol& symbol) const
-    {
-        if (trans_empty())
-            return StateSet{};
-
-        StateSet res;
-        for (auto state : states)
-            for (const auto& symStates : transitionrelation[state])
-                if (symStates.symbol == symbol)
-                    res.insert(symStates.states_to);
-
-        return res;
-    }
+    StateSet post(const StateSet& states, const Symbol& symbol) const;
 
     /**
      * Get shortest words (regarding their length) of the automaton using BFS.
