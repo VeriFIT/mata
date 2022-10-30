e97cf25f0c2906724c201b0f31c31178ae6a1eb5
Lukas Holik <kiloh.sakul@gmail.com>
2022-10-30 18:31:27 +0100

    git mi meni zivot, nevim co delam

--- cmake-build-debug/cli/version.cc
+++ cmake-build-debug/cli/version.cc
@@ -5,7 +5,7 @@
 const char* MATA_VERSION = "2.0";
 
 /// The git SHA of the commit
-const char* MATA_GIT_SHA = "9f63579d8eb93897069ab2b7474575bc8be13f17";
+const char* MATA_GIT_SHA = "c94684a214428c8fdce94bfd3c33b02a45ae66e8";
 
 /// The git --describe output
 const char* MATA_GIT_DESCRIBE = "-128-NOTFOUND";
