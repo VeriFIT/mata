8db99b03abd8e76af09967bd27aff3e152639ab0
Lukas Holik <kiloh.sakul@gmail.com>
2022-12-13 13:09:59 +0100

    in NumberPredicate, resize instead of while loop

--- include/mata/number-predicate.hh
+++ include/mata/number-predicate.hh
@@ -13,6 +13,21 @@
 namespace Mata {
     namespace Util {
 
+        /**
+         * A sort of enhanced boolean array,
+         * implementing a set of numbers, aka a unary predicate over numbers, that provides a constant test and update.
+         * A number that is explicitly added is in the set, all the other numbers are implicitly not in the set.
+         *
+         * Besides a vector of bools (predicate), it can also be asked to maintain a vector of elements (elements).
+         * To keep constant test and set, new elements are pushed back to the vector but remove does not modify the vector.
+         * Hence, after a remove, the vector contains a superset of the true elements.
+         * Superset is still useful, to iterate through true elements, iterate through the vector and test membership in the bool array.
+         * elements_watched indicates that elements are tracked in the vector of elements as described above.
+         * elements_exact indicates that the vector of elements contains the exact set of true elements.
+         * INVARIANT:
+         *  elements_watched -> elements contains a superset of the true elements
+         *  elements_exact -> elements contain exactly the true elements
+         */
         template <class Number> class OrdVector;
 
         template<typename Number>
@@ -21,11 +36,14 @@ namespace Mata {
             mutable std::vector<bool> predicate = {};
             mutable std::vector <Number> elements = {};
             mutable bool elements_exact = true;
-            mutable bool elements_watched = true; //invariant: implies that elements overapproximate the true values
-            //TODO: if it is never used with elements_watched = false, then we can simplify.
+            mutable bool elements_watched = true;
+            //TODO: if it is never used with elements_watched = false, then we remove that and simplify.
 
             using const_iterator = typename std::vector<Number>::const_iterator;
 
+            /**
+             * assuming that elements contain a superset of the true elements, we prune them (in situ)
+             */
             void prune_elements() const {
                 Number new_pos = 0;
                 for (Number orig_pos = 0; orig_pos < elements.size(); ++orig_pos) {
@@ -38,6 +56,9 @@ namespace Mata {
                 elements_exact = true;
             }
 
+            /**
+             * assuming nothing, we compute the true elements by going through the entire bool array
+             */
             void compute_elements() const {
                 elements.clear();
                 for (Number q = 0; q < predicate.size(); ++q) {
@@ -47,6 +68,9 @@ namespace Mata {
                 elements_exact = true;
             }
 
+            /**
+             * calls pruning_elements or compute_elements based on the state of the indicator variables
+             */
             void update_elements() const {
                 if (!elements_exact) {
                     if (!elements_watched) {
@@ -88,8 +112,8 @@ namespace Mata {
             }
 
             void add(Number q) {
-                while (predicate.size() <= q)
-                    predicate.push_back(false);
+                if (predicate.size() <= q)
+                    predicate.resize(q+1,false);
                 if (elements_watched) {
                     Number q_was_there = predicate[q];
                     predicate[q] = true;
@@ -119,6 +143,9 @@ namespace Mata {
                     remove(q);
             }
 
+            /**
+             * start watching elements (might require updating them to establish the invariant)
+             */
             void watch_elements() {
                 if (!elements_watched) {
                     update_elements();
@@ -130,6 +157,9 @@ namespace Mata {
                 elements_watched = false;
             }
 
+            /**
+             * Note that it returns false if q is out of range of the predicate.
+             */
             bool operator[](Number q) const {
                 if (q < predicate.size())
                     return predicate[q];
@@ -137,6 +167,9 @@ namespace Mata {
                     return false;
             }
 
+            /**
+             * This is the number of the true elements, not the size of any data structure.
+             */
             Number size() const {
                 if (elements_exact)
                     return elements.size();
@@ -153,6 +186,9 @@ namespace Mata {
                 }
             }
 
+            /*
+             * clears the set of true elements. Does not clear the predicate, only sets it false everywhere.
+             */
             void clear() {
                 if (elements_watched)
                     for (size_t i = 0; i < elements.size(); i++)
@@ -178,6 +214,10 @@ namespace Mata {
                     remove(q);
             }
 
+            /*
+             * Complements the set with respect to a given number of elements = the maximum number + 1.
+             * Should be somewhat efficient.
+             */
             void complement(Number domain_size) {
                 Number old_domain_size = predicate.size();
                 predicate.reserve(domain_size);
@@ -198,6 +238,9 @@ namespace Mata {
                 }
             }
 
+            /*
+             * Iterators to iterate through the true elements. No order can be assumed.
+             */
             inline const_iterator begin() const {
                 update_elements();
                 return elements.begin();
