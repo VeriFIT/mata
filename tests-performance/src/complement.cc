9a1192c00446f2d4baa3111b7261de7e81fa32e1
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2023-03-31 17:26:19 +0200

    tests-performance: add basic performance testing

---
+++ tests-performance/src/complement.cc
@@ -0,0 +1,69 @@
+#include <mata/inter-aut.hh>
+#include <mata/nfa.hh>
+#include <mata/mintermization.hh>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <cstring>
+
+using namespace Mata::Nfa;
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		std::cerr << "Input file missing\n";
+		return EXIT_FAILURE;
+	}
+
+	std::string filename = argv[1];
+
+	std::fstream fs(filename, std::ios::in);
+	if (!fs) {
+		std::cerr << "Could not open file \'" << filename << "'\n";
+		return EXIT_FAILURE;
+	}
+
+	Mata::Parser::Parsed parsed;
+	Nfa aut;
+    Mata::StringToSymbolMap stsm;
+	const char* nfa_str = "NFA";
+	try {
+		parsed = Mata::Parser::parse_mf(fs, true);
+		fs.close();
+
+		if (parsed.size() != 1) {
+			throw std::runtime_error(
+				"The number of sections in the input file is not 1\n");
+		}
+		if (strncmp(parsed[0].type.c_str(), nfa_str, strlen(nfa_str)) != 0) {
+			throw std::runtime_error("The type of input automaton is not NFA\n");
+		}
+
+        std::vector<Mata::IntermediateAut> inter_auts = Mata::IntermediateAut::parse_from_mf(parsed);
+        Mata::Mintermization mintermization;
+        auto mintermized = mintermization.mintermize(inter_auts);
+        assert(mintermized.size() == 1);
+		aut = construct(mintermized[0], &stsm);
+	}
+	catch (const std::exception& ex) {
+		fs.close();
+		std::cerr << "libMATA error: " << ex.what() << "\n";
+		return EXIT_FAILURE;
+	}
+
+    Mata::OnTheFlyAlphabet alph{ stsm };
+    auto start = std::chrono::system_clock::now();
+    complement(aut, alph);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "complement: " << elapsed.count() << "\n";
+
+    start = std::chrono::system_clock::now();
+    complement(aut, alph, {{"algorithm", "classical"}, {"minimize", "true"}});
+    end = std::chrono::system_clock::now();
+    elapsed = end - start;
+    std::cout << "complement-and-minimize: " << elapsed.count() << "\n";
+
+	return EXIT_SUCCESS;
+}
