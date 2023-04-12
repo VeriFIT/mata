3712c2ac89092725593c6bf5387b3c36e81ecc2d
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2023-04-12 17:06:29 +0200

    test-perf: update scripts

--- tests-performance/scripts/run_massif.sh
+++ tests-performance/scripts/run_massif.sh
@@ -1,4 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
-valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out $@
-grep mem_heap_B massif.out | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1 | sed 's/^/memory_peak: /'
+print_usage() {
+  echo "usage: ./run_massif.sh [OPTIONS] CMD"
+  echo ""
+  echo "OPTIONS:"
+  echo "  -o, --output-file    Saves massif output to file"
+}
+
+outfile=
+
+while [[ "$#" -gt 0 ]];
+do
+    key="$1"
+
+    case $key in
+        --help|-h)
+            print_usage
+            exit 0
+            ;;
+        --output-file|-o)
+            outfile="$2"
+            shift 2
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+cmd=$@
+
+if [ -z "$outfile" ]
+then
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    head=$(git rev-parse --short HEAD)
+    escaped_cmd=$(printf '%q' "${cmd//[\/ ]/-}")
+    outfile="massif.out.$branch-$head-$escaped_cmd"
+fi
+
+valgrind --tool=massif --pages-as-heap=yes --massif-out-file="$outfile" $cmd
+grep mem_heap_B "$outfile" | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1 | sed 's/^/memory_peak: /'
+echo ""
+echo "== Output saved to '$outfile' =="
