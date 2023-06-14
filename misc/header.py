4f390a199d89402133d61809d2dfdad90660868a
David Chocholatý <chocholaty.david@protonmail.com>
2023-06-14 08:34:55 +0200

    Prepare new file headers

---
+++ misc/header.py
@@ -0,0 +1,10 @@
+"""
+<Brief description of the file.>
+
+<(Optional) More elaborate description of the file.>
+
+Mata: An automata library.
+Copyright (C) 2022-2023, Mata group and contributors.
+Distributed under the MIT License.
+(See accompanying file LICENSE or copy at https://opensource.org/license/mit/.)
+"""
