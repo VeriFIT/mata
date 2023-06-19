3b81c9c5276323ca8c32c5268b8ed89034bfcded
Lukas Holik <kiloh.sakul@gmail.com>
2023-06-20 00:46:28 +0200

    comment

--- include/mata/sparse-set.hh
+++ include/mata/sparse-set.hh
@@ -324,6 +324,7 @@ namespace Mata::Util {
             assert(consistent());
         }
 
+        // This could be a template with types of A and B both as parameters, in Util?
         friend bool are_disjoint(const SparseSet & A, const SparseSet & B) {
             if (A.size() > B.size())
                 return are_disjoint(B,A);
