c5e49bf4f59780099713d7a5ac63788c808316f4
vhavlena <vohavlena@gmail.com>
2023-06-23 08:47:31 +0200

    Move::insert optimization

--- src/nfa/nfa.cc
+++ src/nfa/nfa.cc
@@ -2288,6 +2288,10 @@ Move& Move::operator=(Move&& rhs) noexcept {
 }
 
 void Move::insert(State s) {
+    if(targets.empty() || targets.back() < s) {
+        targets.push_back(s);
+        return;
+    }
     // Find the place where to put the element (if not present).
     // insert to OrdVector without the searching of a proper position inside insert(const Key&x).
     auto it = std::lower_bound(targets.begin(), targets.end(), s);
