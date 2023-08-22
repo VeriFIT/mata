7c30c8761b506b4c232a2db724f8c9bc958dbc5c
Tomas Fiedor <ifiedortom@fit.vutbr.cz>
2023-08-22 13:43:37 +0200

    benchmarking: add sources for b-regex and b-incl

--- tests-integration/src/bench-email-filter.cc
+++ tests-integration/src/bench-email-filter.cc
@@ -1,13 +1,13 @@
 /**
- * Benchmark: Automata Inclusion (b-armc-incl)
+ * Benchmark: Email Filter (b-regex)
  *
- * The benchmark program reproduces the results of CADE'23 for benchmarks in directory /nfa-bench/benchmarks/automata_inclusion
+ * The benchmark program reproduces the results of CADE'23 for benchmarks in directory /nfa-bench/benchmarks/email_filter
  *
- * Optimal Inputs: inputs/bench-double-automata-inclusion.in
+ * Optimal Inputs: inputs/bench-quintuple-email-filter.in
  *
  * The original benchmark had the following statistics:
- *   1. Average: 1.9s
- *   2. Median: 0.8s
+ *   1. Average: 0.2s
+ *   2. Median: 0.1s
  *   3. Timeouts: 0
  *
  * NOTE: Input automata, that are of type `NFA-bits` are mintermized!
@@ -15,38 +15,41 @@
  */
 
 #include "utils/utils.hh"
+#include "mata/nfa/algorithms.hh"
 
 constexpr bool MINTERMIZE_AUTOMATA{ true};
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 6) {
         std::cerr << "Input files missing\n";
         return EXIT_FAILURE;
     }
 
-    std::vector<std::string> filenames {argv[1], argv[2]};
+    std::vector<std::string> filenames {
+        argv[1], argv[2], argv[3], argv[4], argv[5]
+    };
     std::vector<Nfa> automata;
     Mata::OnTheFlyAlphabet alphabet;
     if (load_automata(filenames, automata, alphabet, MINTERMIZE_AUTOMATA) != EXIT_SUCCESS) {
         return EXIT_FAILURE;
     }
-    // This might be less-efficient, but more readable.
-    Nfa lhs = automata[0];
-    Nfa rhs = automata[1];
 
     ParameterMap params;
 
     // Setting precision of the times to fixed points and 4 decimal places
     std::cout << std::fixed << std::setprecision(4);
 
+    Nfa intersect_aut;
     params["algorithm"] = "naive";
     TIME_BEGIN(automata_inclusion_naive);
-    Mata::Nfa::is_included(lhs, rhs, &alphabet, params);
+    intersect_aut = intersection(intersection(intersection(automata[0], automata[1]), automata[2]), automata[3]);
+    is_included(automata[4], intersect_aut, &alphabet, params);
     TIME_END(automata_inclusion_naive);
 
     params["algorithm"] = "antichains";
     TIME_BEGIN(automata_inclusion_antichain);
-    Mata::Nfa::is_included(lhs, rhs, &alphabet, params);
+    intersect_aut = intersection(intersection(intersection(automata[0], automata[1]), automata[2]), automata[3]);
+    is_included(automata[4], intersect_aut, &alphabet, params);
     TIME_END(automata_inclusion_antichain);
 
     return EXIT_SUCCESS;
