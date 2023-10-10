0de6c850deccbd50a06da2e8b3c5ee017c842b6d
David Chocholatý <chocholaty.david@protonmail.com>
2023-10-10 09:59:02 +0200

    Polish unifying targets

--- src/nfa/delta.cc
+++ src/nfa/delta.cc
@@ -680,10 +680,13 @@ Symbol Delta::get_max_symbol() const {
 }
 
 StateSet SynchronizedExistentialSymbolPostIterator::unify_targets() const {
+    // TODO: decide which version performs the best.
+
     if(!is_synchronized()) { return {}; }
 
     StateSet unified_targets{};
 
+    // Version with synchronized iterator.
     // static utils::SynchronizedExistentialIterator<StateSet::const_iterator> sync_iterator;
     // sync_iterator.reset();
     // size_t all_targets_size{ 0 };
@@ -696,33 +699,28 @@ StateSet SynchronizedExistentialSymbolPostIterator::unify_targets() const {
     // unified_targets.reserve(all_targets_size);
     // while (sync_iterator.advance()) { unified_targets.push_back(*sync_iterator.get_current_minimum()); }
 
-    using QueueItem = std::pair<typename StateSet::const_iterator, typename StateSet::const_iterator>;
-    auto compare = [](const auto& a, const auto& b) { return *(a.first) > *(b.first); };
-    std::priority_queue<QueueItem, std::vector<QueueItem>, decltype(compare) > heap(compare);
-
-    // size_t max{ 0 };
-    auto& current{ get_current() };
-    for (const auto& r: current) {
-        // max = std::max(max, r->targets.size());
-        heap.emplace(r->cbegin(), r->cend());
-    }
-    unified_targets.reserve(32);
-
-    while (!heap.empty()) {
-        auto item = heap.top();
-        heap.pop();
-        if (unified_targets.empty() || unified_targets.back() != *(item.first)) {
-            unified_targets.push_back(*(item.first));
-        }
-
-        if (++item.first != item.second) {
-            heap.emplace(item);
-        }
-    }
-
+    // Version with set union.
     // for (const auto& symbol_post_it: get_current()) {
     //     unified_targets.insert(symbol_post_it->targets);
     // }
+
+    // Version with priority queue.
+    using TargetSetBeginEndPair = std::pair<StateSet::const_iterator, StateSet::const_iterator>;
+    auto compare = [](const auto& a, const auto& b) { return *(a.first) > *(b.first); };
+    std::priority_queue<TargetSetBeginEndPair, std::vector<TargetSetBeginEndPair>, decltype(compare) > queue(compare);
+    for (const StatePost::const_iterator& symbol_post_it: get_current()) {
+        queue.emplace(symbol_post_it->cbegin(), symbol_post_it->cend());
+    }
+    unified_targets.reserve(32);
+    while (!queue.empty()) {
+        auto item = queue.top();
+        queue.pop();
+        if (unified_targets.empty() || unified_targets.back() != *(item.first)) {
+            unified_targets.push_back(*(item.first));
+        }
+        if (++item.first != item.second) { queue.emplace(item); }
+    }
+
     return unified_targets;
 }
 
