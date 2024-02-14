608146a0c4e74722266763b5cd8518221e8e3597
David Chocholatý <chocholaty.david@protonmail.com>
2024-02-14 10:37:40 +0100

    Create identity with single replace

--- include/mata/lvlfa/strings.hh
+++ include/mata/lvlfa/strings.hh
@@ -13,6 +13,12 @@ namespace mata::lvlfa {
  */
 Lvlfa create_identity(mata::Alphabet* alphabet, Level level_cnt = 2);
 
+/**
+ * Create identity input/output transducer with 2 levels over the @p alphabet with @p level_cnt levels with single
+ *  symbol @p from_symbol replaced with @to_symbol.
+ */
+Lvlfa create_identity_with_single_replace(mata::Alphabet* alphabet, Symbol from_symbol, Symbol to_symbol);
+
 } // Namespace mata::lvlfa.
 
 #endif // MATA_LVLFA_STRING_SOLVING_HH_.
--- tests/lvlfa/strings.cc
+++ tests/lvlfa/strings.cc
@@ -91,3 +91,48 @@ TEST_CASE("lvlfa::create_identity()") {
         CHECK(nft_identity.is_identical(nft));
     }
 }
+
+TEST_CASE("lvlfa::create_identity_with_single_replace()") {
+    Lvlfa nft{};
+    nft.initial = { 0 };
+    nft.final = { 0 };
+    SECTION("small identity nft") {
+        EnumAlphabet alphabet{ 0, 1, 2, 3 };
+        nft.alphabet = &alphabet;
+        nft.delta.add(0, 0, 1);
+        nft.delta.add(1, 0, 0);
+        nft.delta.add(0, 1, 2);
+        nft.delta.add(2, 3, 0);
+        nft.delta.add(0, 2, 3);
+        nft.delta.add(3, 2, 0);
+        nft.delta.add(0, 3, 4);
+        nft.delta.add(4, 3, 0);
+        nft.levels_cnt = 2;
+        nft.levels.resize(5);
+        nft.levels[0] = 0;
+        nft.levels[1] = 1;
+        nft.levels[2] = 1;
+        nft.levels[3] = 1;
+        nft.levels[4] = 1;
+        Lvlfa nft_identity_with_replace{ create_identity_with_single_replace(&alphabet, 1, 3) };
+        CHECK(nft_identity_with_replace.is_identical(nft));
+    }
+
+    SECTION("identity nft no symbols") {
+        EnumAlphabet alphabet{};
+        CHECK_THROWS(create_identity_with_single_replace(&alphabet, 1, 2));
+    }
+
+    SECTION("identity nft one symbol") {
+        EnumAlphabet alphabet{ 0 };
+        nft.alphabet = &alphabet;
+        nft.levels_cnt = 2;
+        nft.levels.resize(2);
+        nft.levels[0] = 0;
+        nft.levels[1] = 1;
+        nft.delta.add(0, 0, 1);
+        nft.delta.add(1, 1, 0);
+        Lvlfa nft_identity{ create_identity_with_single_replace(&alphabet, 0, 1) };
+        CHECK(nft_identity.is_identical(nft));
+    }
+}
