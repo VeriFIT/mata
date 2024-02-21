1431df35bde3d458c515a9623935537b32a4baf4
David Chocholatý <chocholaty.david@protonmail.com>
2024-02-21 08:52:41 +0100

    Create reluctant NFA with marker

--- include/mata/nft/strings.hh
+++ include/mata/nft/strings.hh
@@ -46,6 +46,7 @@ nfa::Nfa begin_marker_nfa(nfa::Nfa regex, Alphabet* alphabet);
 
 Nft begin_marker_nft(const nfa::Nfa& begin_marker_dfa, Symbol begin_marker);
 Nft end_marker_dft(const nfa::Nfa& end_marker_dfa, Symbol end_marker);
+nfa::Nfa reluctant_nfa_with_marker(nfa::Nfa nfa, Symbol marker, Alphabet* alphabet);
 } // Namespace mata::nft::strings.
 
 #endif // MATA_NFT_STRING_SOLVING_HH_.
