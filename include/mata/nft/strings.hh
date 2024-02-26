c8f4ee72557edaae71e9dcb8fe09da0d1b333868
David Chocholatý <chocholaty.david@protonmail.com>
2024-02-26 13:49:37 +0100

    Add reluctant replace for literal

--- include/mata/nft/strings.hh
+++ include/mata/nft/strings.hh
@@ -36,6 +36,14 @@ Nft create_identity_with_single_symbol_replace(mata::Alphabet* alphabet, Symbol 
 Nft create_identity_with_single_symbol_replace(mata::Alphabet* alphabet, Symbol from_symbol, const Word& replacement,
                                                ReplaceMode replace_mode = ReplaceMode::All);
 
+Nft replace_reluctant(
+    const Word& literal,
+    const Word& replacement,
+    Alphabet* alphabet,
+    // TODO(nft): Change into constants?
+    ReplaceMode replace_mode,
+    Symbol begin_marker = EPSILON - 100
+);
 
 Nft replace_reluctant(
     const std::string& regex,
@@ -45,6 +53,7 @@ Nft replace_reluctant(
     ReplaceMode replace_mode,
     Symbol begin_marker = EPSILON - 100
 );
+
 Nft replace_reluctant(
     nfa::Nfa regex,
     const Word& replacement,
@@ -52,6 +61,12 @@ Nft replace_reluctant(
     ReplaceMode replace_mode,
     Symbol begin_marker = EPSILON - 100
 );
+
+Nft replace_reluctant_finite(nfa::Nfa regex, const Word& replacement, Alphabet* alphabet,
+                             ReplaceMode replace_mode, Symbol end_marker = EPSILON - 99);
+
+Nft replace_reluctant_literal(const Word& literal, const Word& replacement, Alphabet* alphabet,
+                              ReplaceMode replace_mode, Symbol end_marker = EPSILON - 99);
 
 nfa::Nfa end_marker_dfa(nfa::Nfa regex);
 Nft marker_nft(const nfa::Nfa& marker_dfa, Symbol marker);
@@ -68,6 +83,9 @@ nfa::Nfa reluctant_nfa_with_marker(nfa::Nfa nfa, Symbol marker, Alphabet* alphab
 
 Nft reluctant_leftmost_nft(const std::string& regex, Alphabet* alphabet, Symbol begin_marker, const Word& replacement, ReplaceMode replace_mode);
 Nft reluctant_leftmost_nft(nfa::Nfa nfa, Alphabet* alphabet, Symbol begin_marker, const Word& replacement, ReplaceMode replace_mode);
+
+Nft replace_literal_nft(const Word& literal, const Word& replacement, const Alphabet* alphabet, const Symbol end_marker,
+                        ReplaceMode replace_mode = ReplaceMode::All);
 } // Namespace mata::nft::strings.
 
 #endif // MATA_NFT_STRING_SOLVING_HH_.
