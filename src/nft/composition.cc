40498259a9e779af76f4a3ac0e2f606727bc4f9c
koniksedy <xsedym02@vutbr.cz>
2024-03-05 18:59:31 +0100

    mata::nft::ntersection() supports jump symbol repetition; JumpMode enum introduced

--- src/nft/composition.cc
+++ src/nft/composition.cc
@@ -12,7 +12,7 @@ using namespace mata::utils;
 namespace mata::nft
 {
 
-Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_levels, const OrdVector<Level>& rhs_sync_levels) {
+Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_levels, const OrdVector<Level>& rhs_sync_levels, const JumpMode jump_mode) {
     assert(!lhs_sync_levels.empty());
     assert(lhs_sync_levels.size() == rhs_sync_levels.size());
 
@@ -82,8 +82,8 @@ Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_lev
     lhs_new_levels_mask.insert(lhs_new_levels_mask.end(), biggest_suffix_len - lhs_suffix_len, true);
     rhs_new_levels_mask.insert(rhs_new_levels_mask.end(), biggest_suffix_len - rhs_suffix_len, true);
 
-    Nft lhs_synced = insert_levels(lhs, lhs_new_levels_mask, DONT_CARE, false);
-    Nft rhs_synced = insert_levels(rhs, rhs_new_levels_mask, DONT_CARE, false);
+    Nft lhs_synced = insert_levels(lhs, lhs_new_levels_mask, DONT_CARE, jump_mode);
+    Nft rhs_synced = insert_levels(rhs, rhs_new_levels_mask, DONT_CARE, jump_mode);
 
     // Two auxiliary states (states from inserted loops) can not create a product state.
     const State lhs_first_aux_state = lhs_synced.num_of_states();
@@ -92,13 +92,13 @@ Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_lev
     insert_self_loops(lhs_synced, lhs_new_levels_mask);
     insert_self_loops(rhs_synced, rhs_new_levels_mask);
 
-    Nft result{ intersection(lhs_synced, rhs_synced, nullptr, lhs_first_aux_state, rhs_first_aux_state) };
-    result = project_out(result, levels_to_project_out, false);
+    Nft result{ intersection(lhs_synced, rhs_synced, nullptr, jump_mode, lhs_first_aux_state, rhs_first_aux_state) };
+    result = project_out(result, levels_to_project_out, jump_mode);
     return result;
 }
 
-Nft compose(const Nft& lhs, const Nft& rhs, const Level lhs_sync_level, const Level rhs_sync_level) {
-    return compose(lhs, rhs, OrdVector{ lhs_sync_level }, OrdVector{ rhs_sync_level });
+Nft compose(const Nft& lhs, const Nft& rhs, const Level lhs_sync_level, const Level rhs_sync_level, const JumpMode jump_mode) {
+    return compose(lhs, rhs, OrdVector{ lhs_sync_level }, OrdVector{ rhs_sync_level }, jump_mode);
 }
 
 } // mata::nft
