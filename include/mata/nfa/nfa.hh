77d43524dabfcdbba9d3a1155946a4b467648c33
David Chocholatý <chocholaty.david@protonmail.com>
2024-07-04 13:11:31 +0200

    fix: Make Nfa::get_words() a const method

--- include/mata/nfa/nfa.hh
+++ include/mata/nfa/nfa.hh
@@ -350,7 +350,7 @@ public:
      * you can get all words by calling
      *      get_words(aut.num_of_states())
      */
-    std::set<Word> get_words(unsigned max_length);
+    std::set<Word> get_words(unsigned max_length) const;
 
     /**
      * @brief Get any arbitrary accepted word in the language of the automaton.
