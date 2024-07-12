9769ef3f99329c0c563fb39f56c8337ad90349fd
David Chocholatý <chocholaty.david@protonmail.com>
2024-07-12 09:33:39 +0200

    Revert "feat: Allow getting symbols to work with from variadic number of NFAs"
    This reverts commit 5ad3348f489d4d0064488fcbb36b48cadd53af64.

--- include/mata/nfa/nfa.hh
+++ include/mata/nfa/nfa.hh
@@ -695,11 +695,9 @@ Run encode_word(const Alphabet* alphabet, const std::vector<std::string>& input)
 
 /**
  * Get the set of symbols to work with during operations.
- * @param[in] nfas Variadic number of NFAs to get symbols from.
  * @param[in] shared_alphabet Optional alphabet shared between NFAs passed as an argument to a function.
  */
-template<typename... Nfas, typename = AreAllOfType<const Nfa&, Nfas...>>
-utils::OrdVector<Symbol> get_symbols_to_work_with(const Alphabet* const shared_alphabet = nullptr, const Nfas&... nfas);
+utils::OrdVector<Symbol> get_symbols_to_work_with(const nfa::Nfa& nfa, const Alphabet* const shared_alphabet = nullptr);
 
 } // namespace mata::nfa.
 
