0b5ac536f3ea92442ee5f943942fcd3f444af192
jurajsic <juraj.sic@outlook.com>
2025-03-10 12:51:40 +0100

    added function that creates Nft from Nfa with specified num_of_levels

--- include/mata/nft/nft.hh
+++ include/mata/nft/nft.hh
@@ -155,6 +155,21 @@ public:
     Nft& operator=(mata::nfa::Nfa&& other) noexcept;
 
     /**
+     * @brief Creates Nft from @p nfa with specified @p num_of_levels automatically.
+     * 
+     * It assumes that @p nfa is a representation of an nft without jump transitions.
+     * It assign to each state the level based on the distance from the initial state.
+     * For example, if there are 2 levels, the initial states are level 0, the following
+     * states are level 1, the states after that level 0, etc.
+     * 
+     * If you only have one level, then it is more efficient to call the constructor that
+     * takes Nfa as input.
+     * 
+     * @throws std::runtime_error if some state should be assigned two different levels
+     */
+    static Nft from_nfa_leveled(mata::nfa::Nfa nfa, size_t num_of_levels);
+
+    /**
      * Add a new (fresh) state to the automaton.
      * @return The newly created state.
      */
