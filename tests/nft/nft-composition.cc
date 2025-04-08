ab7fc5e8e18e253c36e01351c050d87c2697ccfd
koniksedy <xsedym02@vutbr.cz>
2025-04-08 18:02:41 +0200

    new test

--- tests/nft/nft-composition.cc
+++ tests/nft/nft-composition.cc
@@ -420,4 +420,54 @@ TEST_CASE("Mata::nft::compose()") {
             CHECK(are_equivalent(result, expected));
         }
     }
+
+    SECTION("level ordering") {
+        lhs = Nft(13, { 0 }, { 12 }, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0 }, 12);
+        lhs.delta.add(0, 'u', 1);
+        lhs.delta.add(1, 'a', 2);
+        lhs.delta.add(2, 'b', 3);
+        lhs.delta.add(3, 'v', 4);
+        lhs.delta.add(4, 'w', 5);
+        lhs.delta.add(5, 'c', 6);
+        lhs.delta.add(6, 'x', 7);
+        lhs.delta.add(7, 'y', 8);
+        lhs.delta.add(8, 'd', 9);
+        lhs.delta.add(9, 'e', 10);
+        lhs.delta.add(10, 'z', 11);
+        lhs.delta.add(11, 'f', 12);
+
+        rhs = Nft(14, { 0 }, { 13 }, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0 }, 13);
+        rhs.delta.add(0, 'g', 1);
+        rhs.delta.add(1, 'h', 2);
+        rhs.delta.add(2, 'u', 3);
+        rhs.delta.add(3, 'i', 4);
+        rhs.delta.add(4, 'v', 5);
+        rhs.delta.add(5, 'w', 6);
+        rhs.delta.add(6, 'j', 7);
+        rhs.delta.add(7, 'x', 8);
+        rhs.delta.add(8, 'y', 9);
+        rhs.delta.add(9, 'k', 10);
+        rhs.delta.add(10, 'l', 11);
+        rhs.delta.add(11, 'm', 12);
+        rhs.delta.add(12, 'z', 13);
+
+        expected = Nft(14, { 0 }, { 13 }, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0 }, 13);
+        expected.delta.add(0, 'g', 1);
+        expected.delta.add(1, 'h', 2);
+        expected.delta.add(2, 'a', 3);
+        expected.delta.add(3, 'b', 4);
+        expected.delta.add(4, 'i', 5);
+        expected.delta.add(5, 'c', 6);
+        expected.delta.add(6, 'j', 7);
+        expected.delta.add(7, 'd', 8);
+        expected.delta.add(8, 'e', 9);
+        expected.delta.add(9, 'k', 10);
+        expected.delta.add(10, 'l', 11);
+        expected.delta.add(11, 'm', 12);
+        expected.delta.add(12, 'f', 13);
+
+        result = compose(lhs, rhs, { 0, 3, 4, 6, 7, 10 }, { 2, 4, 5, 7, 8, 12 });
+
+        CHECK(are_equivalent(result, expected));
+    }
 }
