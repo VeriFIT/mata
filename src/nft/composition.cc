e95a3396091e52b9ac30f058ba5bcfc6727500c2
koniksedy <xsedym02@vutbr.cz>
2025-04-08 17:56:06 +0200

    working draft

--- src/nft/composition.cc
+++ src/nft/composition.cc
@@ -35,53 +35,69 @@ Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_lev
         }
     };
 
-    // Calculate new_levels_mask, which is used for inserting levels to match the synchronization
-    // transitions in lhs and rhs. It also calculates the vector levels_to_project_out, which contains
-    // levels of synchronized transitions. These levels will be projected out from the Nft after the composition.
+    // Calculate the number of non-synchronization levels in lhs and rhs before/after each/last synchronization level.
     // Example:
-    // lhs_sync_levels: 1 4
-    // rhs_sync_levels: 2 3
-    // lhs_new_levels_mask: 0 1 0 0 0
-    // rhs_new_levels_mask: 0 0 1 1 0
-    // levels_to_project_out: 2 5
-    Level min_level = std::min(*lhs_sync_levels.begin(), *rhs_sync_levels.begin());
-    size_t lhs_suffix_len = lhs.num_of_levels - 1 - *--lhs_sync_levels.end();
-    size_t rhs_suffix_len = rhs.num_of_levels - 1 - *--rhs_sync_levels.end();
-    size_t biggest_suffix_len = std::max(lhs_suffix_len, rhs_suffix_len);
-    BoolVector lhs_new_levels_mask(min_level, false);
-    BoolVector rhs_new_levels_mask(min_level, false);
-    OrdVector<Level> levels_to_project_out;
-    Level lhs_offset{ 0 };
-    Level rhs_offset{ 0 };
-    Level lhs_lvl{ 0 };
-    Level rhs_lvl{ 0 };
-    for (auto lhs_sync_levels_it{ lhs_sync_levels.begin() }, rhs_sync_levels_it{ rhs_sync_levels.begin ()};
-         lhs_sync_levels_it != lhs_sync_levels.end();
-         ++lhs_sync_levels_it, ++rhs_sync_levels_it) {
-        lhs_lvl = *lhs_sync_levels_it + lhs_offset;
-        rhs_lvl = *rhs_sync_levels_it + rhs_offset;
-        if (lhs_lvl < rhs_lvl) {
-            lhs_new_levels_mask.insert(lhs_new_levels_mask.end(), rhs_lvl - lhs_lvl, true);
-            rhs_new_levels_mask.insert(rhs_new_levels_mask.end(), rhs_lvl - lhs_lvl, false);
-            lhs_offset += rhs_lvl - lhs_lvl ;
-        } else if (lhs_lvl > rhs_lvl) {
-            lhs_new_levels_mask.insert(lhs_new_levels_mask.end(), lhs_lvl - rhs_lvl, false);
-            rhs_new_levels_mask.insert(rhs_new_levels_mask.end(), lhs_lvl - rhs_lvl, true);
-            rhs_offset = lhs_lvl - rhs_lvl;
-        } else {
-            lhs_new_levels_mask.resize(lhs_lvl, false);
-            rhs_new_levels_mask.resize(rhs_lvl, false);
-        }
-        lhs_new_levels_mask.push_back(false);
-        rhs_new_levels_mask.push_back(false);
-        levels_to_project_out.push_back(static_cast<Level>(lhs_new_levels_mask.size() - 1));
+    //    Lets suppose we have 2 synchnonization levels.
+    //    The vector lhs_nonsync_levels_cnt = { 2, 0, 1 } indicates that
+    //    - before the first synchronization level (i == 0) there are 2 non-synchronization levels
+    //    - before the second synchronization level (i == 1) there are 0 non-synchronization levels
+    //    - after the last synchronization level (i == |sync| - 1) there is 1 non-synchronization level
+    std::vector<unsigned> lhs_nonsync_levels_cnt;
+    std::vector<unsigned> rhs_nonsync_levels_cnt;
+    const size_t num_of_sync_levels = lhs_sync_levels.size();
+    lhs_nonsync_levels_cnt.reserve(num_of_sync_levels + 1);
+    rhs_nonsync_levels_cnt.reserve(num_of_sync_levels + 1);
+    auto lhs_sync_levels_it = lhs_sync_levels.cbegin();
+    auto rhs_sync_levels_it = rhs_sync_levels.cbegin();
+    // Before the first synchronization level (i == 0)
+    lhs_nonsync_levels_cnt.push_back(*lhs_sync_levels_it++);
+    rhs_nonsync_levels_cnt.push_back(*rhs_sync_levels_it++);
+    // Before each synchronization level (i < |sync| - 1)
+    for (size_t i = 1; i < num_of_sync_levels; ++i) {
+        lhs_nonsync_levels_cnt.push_back(*lhs_sync_levels_it - *(lhs_sync_levels_it - 1) - 1);
+        rhs_nonsync_levels_cnt.push_back(*rhs_sync_levels_it - *(rhs_sync_levels_it - 1) - 1);
+        ++lhs_sync_levels_it;
+        ++rhs_sync_levels_it;
     }
-    // Match the size of vectors and num_of_levels in lhs and rhs after the insertion of new levels.
-    lhs_new_levels_mask.insert(lhs_new_levels_mask.end(), lhs_suffix_len, false);
-    rhs_new_levels_mask.insert(rhs_new_levels_mask.end(), rhs_suffix_len, false);
-    lhs_new_levels_mask.insert(lhs_new_levels_mask.end(), biggest_suffix_len - lhs_suffix_len, true);
-    rhs_new_levels_mask.insert(rhs_new_levels_mask.end(), biggest_suffix_len - rhs_suffix_len, true);
+    // After the last synchronization level (i == |sync| - 1)
+    lhs_nonsync_levels_cnt.push_back(static_cast<unsigned>(lhs.num_of_levels) - *(lhs_sync_levels_it - 1) - 1);
+    rhs_nonsync_levels_cnt.push_back(static_cast<unsigned>(rhs.num_of_levels) - *(rhs_sync_levels_it - 1) - 1);
 
+    // Construct a mask for new levels in lhs and rhs.
+    // For each synchronization block (non-synchronization levels up to a synchronization transition),
+    // first insert the non-synchronization levels from lhs, then from rhs, and finally the synchronization level itself.
+    // For the last block of non-synchronization levels after the last synchronization level,
+    // insert the non-synchronization levels from lhs first, followed by the levels from rhs.
+    auto lhs_nonsync_levels_cnt_it = lhs_nonsync_levels_cnt.cbegin();
+    auto rhs_nonsync_levels_cnt_it = rhs_nonsync_levels_cnt.cbegin();
+    BoolVector lhs_new_levels_mask;
+    BoolVector rhs_new_levels_mask;
+    OrdVector<Level> sync_levels_to_project_out;
+    Level level = 0;
+    const size_t nonsync_levels_cnt_size = lhs_nonsync_levels_cnt.size();
+    for (size_t i = 0; i < nonsync_levels_cnt_size; ++i) {
+        // LHS goes first -> make space (insert new levels) in RHS
+        for (unsigned j = 0; j < *lhs_nonsync_levels_cnt_it; ++j, ++level) {
+            lhs_new_levels_mask.push_back(false);
+            rhs_new_levels_mask.push_back(true);
+        }
+        // RHS goes first -> make space (insert new levels) in LHS
+        for (unsigned j = 0; j < *rhs_nonsync_levels_cnt_it; ++j, ++level) {
+            lhs_new_levels_mask.push_back(true);
+            rhs_new_levels_mask.push_back(false);
+        }
+        // The synchronization level goes last
+        if (i < num_of_sync_levels) {
+            sync_levels_to_project_out.push_back(level);
+            lhs_new_levels_mask.push_back(false);
+            rhs_new_levels_mask.push_back(false);
+            ++level;
+        }
+        ++lhs_nonsync_levels_cnt_it;
+        ++rhs_nonsync_levels_cnt_it;
+    }
+
+    // Insert new levels into lhs and rhs.
     Nft lhs_synced = insert_levels(lhs, lhs_new_levels_mask, jump_mode);
     Nft rhs_synced = insert_levels(rhs, rhs_new_levels_mask, jump_mode);
 
@@ -89,12 +105,13 @@ Nft compose(const Nft& lhs, const Nft& rhs, const OrdVector<Level>& lhs_sync_lev
     const State lhs_first_aux_state = lhs_synced.num_of_states();
     const State rhs_first_aux_state = rhs_synced.num_of_states();
 
+    // Insert self-loops into lhs and rhs to ensure synchronization on epsilon transitions.
     insert_self_loops(lhs_synced, lhs_new_levels_mask);
     insert_self_loops(rhs_synced, rhs_new_levels_mask);
 
     Nft result{ intersection(lhs_synced, rhs_synced, nullptr, jump_mode, lhs_first_aux_state, rhs_first_aux_state) };
     if (project_out_sync_levels) {
-        result = project_out(result, levels_to_project_out, jump_mode);
+        result = project_out(result, sync_levels_to_project_out, jump_mode);
     }
     return result;
 }
